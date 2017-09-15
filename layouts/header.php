504c58437e68453faafb40e5dc6f0bdea93f3a8b
SeongbumSeo <sBum.Seo@gmail.com>
2017-09-15 22:41:55 +0900

    header 상단 링크 메뉴

--- layouts/header.php
+++ layouts/header.php
@@ -4,4 +4,9 @@
         <li>Creator page</li>
         <li>Seller page</li>
     </ul>
+    <ul class="menu menu_right">
+        <li>로그아웃</li>
+        <li>마이페이지</li>
+        <li>고객센터</li>
+    </ul>
 </div>
