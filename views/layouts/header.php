dd80a9a22fa034b0a4de69d7fad3e39dc1cc78fa
SeongbumSeo <sBum.Seo@gmail.com>
2017-09-16 14:31:00 +0900

    header 상단 링크 메뉴

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,13 +1,11 @@
 <div class="area_links">
     <ul class="menu menu_left">
         <li>공지사항</li>
-        <li>Creator page</li>
-        <li>Seller page</li>
+        <li>고객센터</li>
     </ul>
     <ul class="menu menu_right">
         <li>로그아웃</li>
         <li>마이페이지</li>
-        <li>고객센터</li>
     </ul>
 </div>
 
