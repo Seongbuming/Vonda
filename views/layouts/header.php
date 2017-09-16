1f5ddc90a23ffa8a917f26f03031a098aaa43c1e
SeongbumSeo <sBum.Seo@gmail.com>
2017-09-16 15:44:56 +0900

    Revert "header 상단 링크 메뉴"
    This reverts commit dd80a9a22fa034b0a4de69d7fad3e39dc1cc78fa.

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,11 +1,13 @@
 <div class="area_links">
     <ul class="menu menu_left">
         <li>공지사항</li>
-        <li>고객센터</li>
+        <li>Creator page</li>
+        <li>Seller page</li>
     </ul>
     <ul class="menu menu_right">
         <li>로그아웃</li>
         <li>마이페이지</li>
+        <li>고객센터</li>
     </ul>
 </div>
 
