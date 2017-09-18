ce74a601a501a01d1c495b99d341c3827bc35b9e
SeongbumSeo <sBum.Seo@gmail.com>
2017-09-18 23:02:01 +0900

    크리에이터 프로필 추가

--- views/home.php
+++ views/home.php
@@ -54,6 +54,49 @@
             <li class="actived"><a href=".">Creator</a></li>
             <li>&nbsp;</li>
         </ul>
+        <div class="category creator">
+            <div class="item">
+                <img src="images/creators/creator1.png" alt="크리에이터 이미지 1" />
+            </div>
+            <div class="item">
+                <img src="images/creators/creator2.png" alt="크리에이터 이미지 2" />
+            </div>
+            <div class="item">
+                <img src="images/creators/creator3.png" alt="크리에이터 이미지 3" />
+            </div>
+            <div class="item">
+                <img src="images/creators/creator4.png" alt="크리에이터 이미지 4" />
+            </div>
+            <div class="creator_detail">
+                <img class="creator_background" src="images/creators/creator_background4.png" alt="크리에이터 배경 이미지" />
+                <div class="creator_image">
+                    <img src="images/creators/creator2.png" alt="크리에이터 이미지" />
+                </div>
+                <div class="creator_profile">
+                    <p class="creator_name">@ANOTHER A</p>
+                    <p class="creator_message">캐쥬얼하고 미니멀한 베이스에 ANOTHER A만의 감성을 더하여 트렌드를 반영함과 동시에 개성 있고 감각적인 룩을 선보입니다. 매 시즌 베이직함을 바탕으로 한 또 다른  디자인들로 다양한 스타일링을 제안합니다.</p>
+                    <div class="icons">
+                        <a href="."><img src="images/icons/home_sns/instagram-logo.png" alt="Instagram" /></a>
+                        <a href="."><img src="images/icons/home_sns/facebook-logo.png" alt="Facebook" /></a>
+                        <a href="."><img src="images/icons/home_sns/twitter-logo.png" alt="Twitter" /></a>
+                        <a href="."><img src="images/icons/home_sns/youtube-logo.png" alt="Youtube" /></a>
+                        <a href="."><img src="images/icons/home_sns/AfreecaTV_logo.png" alt="AfreecaTV" /></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="images/creators/creator5.png" alt="크리에이터 이미지 5" />
+            </div>
+            <div class="item">
+                <img src="images/creators/creator6.png" alt="크리에이터 이미지 6" />
+            </div>
+            <div class="item">
+                <img src="images/creators/creator7.png" alt="크리에이터 이미지 7" />
+            </div>
+            <div class="item">
+                <img src="images/creators/creator8.png" alt="크리에이터 이미지 8" />
+            </div>
+        </div>
     </div>
 
     <footer>
