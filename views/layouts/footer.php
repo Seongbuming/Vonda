794c3ed6798895189a0d70c57e931549d55a46ed
SeongbumSeo <sBum.Seo@gmail.com>
2017-10-01 14:17:36 +0900

    footer

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,1 +1,30 @@
+<div class="area_logo">
+    <img class="logo" src="images/logo.png" alt="로고" />
+</div>
 
+<div class="middle_section">
+    <div class="container">
+        <ul class="area_links">
+            <li><a href=".">[개인정보취급방침]</a></li>
+            <li><a href=".">[이용약관]</a></li>
+        </ul>
+
+        <div class="area_copyright">
+            <p class="title">위머스트크리에이트 크로아상 (Wemustcreate Croasang)<p>
+            <p>대표자명 : 최영호 | 사업자등록번호 : 871-05-00407 | 통신판매업신고번호 : 2016-경기광명-0403호</p>
+            <p>주소 : 경기 광명시 성채안로 26 휴먼시아1단지 104호 706호</p>
+            <p>개인정보관리자 : 최영호 | 전화 : 010-8549-2902 | 이메일 : wemustcreate@naver.com</p>
+        </div>
+    </div>
+</div>
+
+<div class="area_contact">
+    <div class="wrapper">
+        <p class="title">CS CENTER</p>
+        <p>010-8549-2902</p>
+        <p>wemustcreate@naver.com</p>
+        <p>영업시간 AM 9:00 ~ PM 6:00</p>
+        <p>점심시간 PM 12:00 ~ PM 1:00</p>
+        <p>토/일/법정공휴일 휴무</p>
+    </div>
+</div>
