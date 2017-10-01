be4d854c0aacafeb02e877cb1687329c64b6983e
SeongbumSeo <sBum.Seo@gmail.com>
2017-10-01 15:01:02 +0900

    cscenter

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 require("functions/template.php");
 
+$page = $_GET["page"];
+
 $template = new Template();
-$template->loadView("home");
+switch ($page) {
+    case "cscenter":
+        $template->loadView("pages/client/cscenter");
+        break;
+    default:
+        $template->loadView("pages/client/home");
+}
 ?>
---
+++ views/pages/client/cscenter.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+    <?=$this->loadLayout("head")?>
+</head>
+
+<body>
+    <header>
+        <?=$this->loadLayout("header")?>
+    </header>
+
+    <div id="contents">
+        <h2 class="page_title">FAQ</h2>
+    </div>
+    
+    <footer>
+        <?=$this->loadLayout("footer")?>
+    </footer>
+</body>
+</html>
