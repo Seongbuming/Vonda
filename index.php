9d7018dd874f20f602eb48ba1cc2742f779e4cb0
SeongbumSeo <sBum.Seo@gmail.com>
2017-10-03 20:01:03 +0900

    프론트 컨트롤러

--- index.php
+++ index.php
@@ -2,19 +2,9 @@
 require("functions/template.php");
 
 $page = $_GET["page"];
+if (strlen($page) === 0)
+    $page = "home";
 
 $template = new Template();
-switch ($page) {
-    case "cscenter":
-        $template->loadView("pages/client/cscenter");
-        break;
-    case "cart":
-        $template->loadView("pages/client/cart");
-        break;
-    case "searchresults":
-        $template->loadView("pages/client/searchresults");
-        break;
-    default:
-        $template->loadView("pages/client/home");
-}
+$template->loadView("pages/client/$page");
 ?>
