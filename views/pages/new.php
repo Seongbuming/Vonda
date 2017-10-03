a1ad2cdf873844353b13a7e5c2f8283f2cb2533e
SeongbumSeo <sBum.Seo@gmail.com>
2017-10-03 19:58:35 +0900

    new.php

---
+++ views/pages/new.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="ko">
+<head>
+    <?=$this->loadLayout("head")?>
+</head>
+
+<body>
+    <header>
+        <?=$this->loadLayout("header")?>
+    </header>
+
+    <div id="contents">
+    </div>
+
+    <footer>
+        <?=$this->loadLayout("footer")?>
+    </footer>
+</body>
+</html>
