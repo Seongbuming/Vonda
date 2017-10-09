190bfe5040d9d9b644d5b904ea4100f82c2cff0e
SeongbumSeo <sBum.Seo@gmail.com>
2017-10-09 16:19:48 +0900

    indent

--- views/pages/client/creator_profile_boarddetail.php
+++ views/pages/client/creator_profile_boarddetail.php
@@ -8,116 +8,116 @@
 </head>
 
 <body>
-<header>
-    <?= $this->loadLayout("header") ?>
-</header>
-<!--
-<div class="icons">
-                        <a href="."><img src="images/icons/home_sns/instagram-logo.png" alt="Instagram" /></a>
-                        <a href="."><img src="images/icons/home_sns/facebook-logo.png" alt="Facebook" /></a>
-                        <a href="."><img src="images/icons/home_sns/twitter-logo.png" alt="Twitter" /></a>
-                        <a href="."><img src="images/icons/home_sns/youtube-logo.png" alt="Youtube" /></a>
-                        <a href="."><img src="images/icons/home_sns/AfreecaTV_logo.png" alt="AfreecaTV" /></a>
-                    </div>
-                    -->
-<div id="contents">
-    <div class="creator_profile">
-        <img src="images/creators/creator_background1.png" class="creator_profile_bg_img">
-        <img src="images/creators/creator_profile.png" id="profile_location" class="creator_profile_img">
+    <header>
+        <?= $this->loadLayout("header") ?>
+    </header>
 
-        <p class="creator_profile_name">@Yeomim</p>
-        <p class="creator_profile_contents">코튼 소재를 베이스로 한 가볍고 부담 없이 사용하기<br>좋은 가방들을 선보이며 실용성과 함께 스타일리시함도<br>
-            추구하는 것이 브랜드 여밈의 모토입니다.</p>
-    </div>
+    <div id="contents">
+        <!--<div class="icons">
+            <a href="."><img src="images/icons/home_sns/instagram-logo.png" alt="Instagram" /></a>
+            <a href="."><img src="images/icons/home_sns/facebook-logo.png" alt="Facebook" /></a>
+            <a href="."><img src="images/icons/home_sns/twitter-logo.png" alt="Twitter" /></a>
+            <a href="."><img src="images/icons/home_sns/youtube-logo.png" alt="Youtube" /></a>
+            <a href="."><img src="images/icons/home_sns/AfreecaTV_logo.png" alt="AfreecaTV" /></a>
+        </div>-->
 
-    <h3 class="category">BOARD</h3>
-    <div class="profile_detail_board">
-        <div class="post">
-            <p class="creator_title">[브랜드 오픈] 상반된 개념의 믹스앤 매치로 차별화된 컬렉션을 선보이는 레이블, PUSHBUTTO<span class="author"
-                                                                                                 style="float:right; padding-right: 10px;">Yeomim</span>
-            </p>
-            <p class="time">2017.08.22 13:20</p>
-            <div class="creator_detail_result">
-                주말, 집 근처 강가에서 아내 베로니카와 딸 테레사와 함께 `Chick Corea Akoustic Band`의 `Overjoyed`를 들으며,<br>
-                술래잡기하듯 뛰어 노는 이들의 모습을 보며 디자이너는 꼭 신겨주고 싶은 디자인이 생겼습니다.<br>
-                영국 남성 슈즈의 레트로 무드를 바탕으로 스트랩 장식과 감각적인 컷 아웃을 통해 베로니카를 위한 `로퍼 슈즈`로 재현해 보았습니다.<br>
-                <br>
-                우수한 등급의 가죽 소재를 사용하였으며, 소프트한 질감이 시간이 지날수록 당신의 발을 더욱더 자연스럽게 감싸 안을 것입니다.<br>
-                공증된 특허기술을 사용하여 자체 개발된 은은한 향이 나는 라텍스 중창은 편안한 착화감과 동시에 당신의 발을 보다 더 건강하게 지켜줄 것이며,<br>
-                슈즈의 바닥창은 천연 목 굽과 러버를 사용하였습니다.<br>
-                이 슈즈와 당신이 함께 할 시간들이 행복하기를 바랍니다.<br><br>
-                <img src="images/detail/게시물상세.png">
+        <div class="creator_profile">
+            <img src="images/creators/creator_background1.png" class="creator_profile_bg_img">
+            <img src="images/creators/creator_profile.png" id="profile_location" class="creator_profile_img">
+
+            <p class="creator_profile_name">@Yeomim</p>
+            <p class="creator_profile_contents">코튼 소재를 베이스로 한 가볍고 부담 없이 사용하기<br>좋은 가방들을 선보이며 실용성과 함께 스타일리시함도<br>
+                추구하는 것이 브랜드 여밈의 모토입니다.</p>
+        </div>
+
+        <h3 class="category">BOARD</h3>
+        <div class="profile_detail_board">
+            <div class="post">
+                <p class="creator_title">[브랜드 오픈] 상반된 개념의 믹스앤 매치로 차별화된 컬렉션을 선보이는 레이블, PUSHBUTTO<span class="author"
+                                                                                                    style="float:right; padding-right: 10px;">Yeomim</span>
+                </p>
+                <p class="time">2017.08.22 13:20</p>
+                <div class="creator_detail_result">
+                    주말, 집 근처 강가에서 아내 베로니카와 딸 테레사와 함께 `Chick Corea Akoustic Band`의 `Overjoyed`를 들으며,<br>
+                    술래잡기하듯 뛰어 노는 이들의 모습을 보며 디자이너는 꼭 신겨주고 싶은 디자인이 생겼습니다.<br>
+                    영국 남성 슈즈의 레트로 무드를 바탕으로 스트랩 장식과 감각적인 컷 아웃을 통해 베로니카를 위한 `로퍼 슈즈`로 재현해 보았습니다.<br>
+                    <br>
+                    우수한 등급의 가죽 소재를 사용하였으며, 소프트한 질감이 시간이 지날수록 당신의 발을 더욱더 자연스럽게 감싸 안을 것입니다.<br>
+                    공증된 특허기술을 사용하여 자체 개발된 은은한 향이 나는 라텍스 중창은 편안한 착화감과 동시에 당신의 발을 보다 더 건강하게 지켜줄 것이며,<br>
+                    슈즈의 바닥창은 천연 목 굽과 러버를 사용하였습니다.<br>
+                    이 슈즈와 당신이 함께 할 시간들이 행복하기를 바랍니다.<br><br>
+                    <img src="images/detail/게시물상세.png">
+                </div>
+            </div>
+
+            <div class="comment">
+                <textarea cols="156" rows="5" placeholder="댓글을 입력하려면 로그인이 필요합니다."></textarea>
+                <button class="comment_submit">댓글 등록</button>
+            </div>
+            <div class="user_comment">
+                <table class="board">
+                    <tbody>
+                    <tr class="row_subject">
+                        <td class="author">Yeomim</td>
+                        <td class="subject">
+                        너무 기대됩니다. 언능 받아보고 싶어요!</td>
+                        <td class="time">2017.09.21  13:20</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="user_link_comment">
+                <table class="board">
+                    <tbody>
+                    <tr class="row_subject">
+                        <td class="author">ㄴ @Yeomim</td>
+                        <td class="subject">
+                            너무 기대됩니다. 언능 받아보고 싶어요!</td>
+                        <td class="time">2017.09.22  13:20</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="user_comment">
+                <table class="board">
+                    <tbody>
+                    <tr class="row_subject">
+                        <td class="author">haeyfuk</td>
+                        <td class="subject">
+                            너무 기대됩니다. 언능 받아보고 싶어요!</td>
+                        <td class="time">2017.08.21  13:20</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
 
-        <div class="comment">
-            <textarea cols="156" rows="5" placeholder="댓글을 입력하려면 로그인이 필요합니다."></textarea>
-            <button class="comment_submit">댓글 등록</button>
-        </div>
         <div class="user_comment">
             <table class="board">
                 <tbody>
                 <tr class="row_subject">
-                    <td class="author">Yeomim</td>
-                    <td class="subject">
-                       너무 기대됩니다. 언능 받아보고 싶어요!</td>
-                    <td class="time">2017.09.21  13:20</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-
-        <div class="user_link_comment">
-            <table class="board">
-                <tbody>
-                <tr class="row_subject">
-                    <td class="author">ㄴ @Yeomim</td>
+                    <td class="author">jay901004</td>
                     <td class="subject">
                         너무 기대됩니다. 언능 받아보고 싶어요!</td>
-                    <td class="time">2017.09.22  13:20</td>
+                    <td class="time">2017.07.21  13:20</td>
                 </tr>
                 </tbody>
             </table>
         </div>
 
-        <div class="user_comment">
-            <table class="board">
-                <tbody>
-                <tr class="row_subject">
-                    <td class="author">haeyfuk</td>
-                    <td class="subject">
-                        너무 기대됩니다. 언능 받아보고 싶어요!</td>
-                    <td class="time">2017.08.21  13:20</td>
-                </tr>
-                </tbody>
-            </table>
+        <div class="pager">
+            <button class="left">◀</button>
+            <button class="right">▶</button>
         </div>
+        <div class="c_line"></div>
+        <a class="goback" href=".?page=creator_profile">뒤로가기</a>
     </div>
 
-    <div class="user_comment">
-        <table class="board">
-            <tbody>
-            <tr class="row_subject">
-                <td class="author">jay901004</td>
-                <td class="subject">
-                    너무 기대됩니다. 언능 받아보고 싶어요!</td>
-                <td class="time">2017.07.21  13:20</td>
-            </tr>
-            </tbody>
-        </table>
-    </div>
 
-    <div class="pager">
-        <button class="left">◀</button>
-        <button class="right">▶</button>
-    </div>
-    <div class="c_line"></div>
-    <a class="goback" href=".?page=creator_profile">뒤로가기</a>
-</div>
-
-
-<footer>
-    <?= $this->loadLayout("footer") ?>
-</footer>
+    <footer>
+        <?= $this->loadLayout("footer") ?>
+    </footer>
 </body>
 </html>
--- views/pages/client/order.php
+++ views/pages/client/order.php
@@ -6,164 +6,164 @@
 </head>
 
 <body>
-<header>
-    <?= $this->loadLayout("header") ?>
-</header>
+    <header>
+        <?= $this->loadLayout("header") ?>
+    </header>
 
-<div id="contents">
-    <h2 class="page_title">ORDER</h2>
-    <h3 class="category"></h3>
-    <table class="product_list">
-        <thead>
-        <tr>
-            <th>상품명</th>
-            <th width="80px">수량</th>
-            <th width="80px">배송비</th>
-            <th width="100px">주문금액</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>
-                <div class="product_img">
-                    <img src="images/products/product1.png" alt="상품사진"/>
+    <div id="contents">
+        <h2 class="page_title">ORDER</h2>
+        <h3 class="category"></h3>
+        <table class="product_list">
+            <thead>
+                <tr>
+                    <th>상품명</th>
+                    <th width="80px">수량</th>
+                    <th width="80px">배송비</th>
+                    <th width="100px">주문금액</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <div class="product_img">
+                            <img src="images/products/product1.png" alt="상품사진"/>
+                        </div>
+                        <div class="product_info">
+                            <p class="creator"><a href=".">@Creator</a></p>
+                            <p class="name"><a href=".">SINGLE-BREASTED OVERSIZED BLAZER</a></p>
+                            <p class="type">실버</p>
+                        </div>
+                    </td>
+                    <td class="product_amount">1</td>
+                    <td class="product_shippingfee">2,500원</td>
+                    <td class="product_price">26,000원</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="sum_price">
+            <div class="wrapper">
+                <div class="term">
+                    <p class="caption">상품금액</p>
+                    <p class="price">26,000원</p>
                 </div>
-                <div class="product_info">
-                    <p class="creator"><a href=".">@Creator</a></p>
-                    <p class="name"><a href=".">SINGLE-BREASTED OVERSIZED BLAZER</a></p>
-                    <p class="type">실버</p>
+                <div class="operator">+</div>
+                <div class="term">
+                    <p class="caption">배송비</p>
+                    <p class="price">2,500원</p>
                 </div>
-            </td>
-            <td class="product_amount">1</td>
-            <td class="product_shippingfee">2,500원</td>
-            <td class="product_price">26,000원</td>
-        </tr>
-        </tbody>
-    </table>
-    <div class="sum_price">
-        <div class="wrapper">
-            <div class="term">
-                <p class="caption">상품금액</p>
-                <p class="price">26,000원</p>
-            </div>
-            <div class="operator">+</div>
-            <div class="term">
-                <p class="caption">배송비</p>
-                <p class="price">2,500원</p>
-            </div>
-            <div class="operator">=</div>
-            <div class="term">
-                <p class="caption">총 결제금액</p>
-                <p class="price">28,500원</p>
+                <div class="operator">=</div>
+                <div class="term">
+                    <p class="caption">총 결제금액</p>
+                    <p class="price">28,500원</p>
+                </div>
             </div>
         </div>
-    </div>
 
-    <h3 class="category">ORDER</h3>
-    <h4 class="subcategory">주문자 정보</h4>
-    <p class="required_message"><span class="required">*</span>표시는 필수 입력 항목</p>
-    <form class="order_form">
-        <div class="row">
-            <p class="required">*</p>
-            <label for="name">주문자 이름</label>
-            <input id="name" name="name" type="text" class="text" required/>
-        </div>
-        <div class="row pnumber">
-            <p class="required">*</p>
-            <label for="pnumber">연락처</label>
-            <input id="pnumber" name="pnumber_1" type="tel" class="tel" required/>
-            <span>-</span>
-            <input name="pnumber_2" type="tel" class="tel" required/>
-            <span>-</span>
-            <input name="pnumber_3" type="tel" class="tel" required/>
-        </div>
-        <div class="row">
-            <p class="required">*</p>
-            <label for="email">이메일</label>
-            <input id="email" name="email" type="email" class="email" required/>
-            <p class="email_message">이메일로 주문 진행상황을 안내해드립니다.</p>
-        </div>
-    </form>
-
-    <h4 class="subcategory">배송지 정보</h4>
-    <form class="order_form">
-        <div class="row">
-            <p class="required">*</p>
-            <label for="name">수령인 이름</label>
-            <input id="name" name="name" type="text" class="text" required/>
-        </div>
-        <div class="row pnumber">
-            <p class="required">*</p>
-            <label for="pnumber">연락처</label>
-            <input id="pnumber" name="pnumber_1" type="tel" class="tel" required/>
-            <span>-</span>
-            <input name="pnumber_2" type="tel" class="tel" required/>
-            <span>-</span>
-            <input name="pnumber_3" type="tel" class="tel" required/>
-        </div>
-        <div class="row pnumber">
-            <p>&nbsp;&nbsp;</p>
-            <label for="pnumber">추가 연락처</label>
-            <input id="pnumber" name="pnumber_1" type="tel" class="tel" required/>
-            <span>-</span>
-            <input name="pnumber_2" type="tel" class="tel" required/>
-            <span>-</span>
-            <input name="pnumber_3" type="tel" class="tel" required/>
-        </div>
-        <div class="row">
-            <p class="required">*</p>
-            <label for="name">배송지</label>
-            <input id="order" name="order" type="text" class="text" required/>
-            <input class="address_button" type="submit" value="주소 검색">
-            <div class="post_section"><input class="savepost" name="savepost" type="checkbox" checked=""> <label
-                        id="savemsg" for="savepost">기본 배송지로 설정</label></div>
-        </div>
-        <div class="row">
-            <input id="order_big" name="order" type="text" class="text" required/>
-        </div>
-        <div class="row">
-            <input id="order_big" name="order" type="text" class="text" placeholder="상세주소입력" required/>
-        </div>
-        <div class="row">
-            <p>&nbsp;&nbsp;</p>
-            <label for="name">배송 메세지</label>
-            <input id="order_bbig" name="order_msg" type="text" class="text" placeholder="20자 이내로 입력" required/>
-        </div>
-        <div class="row">
-            <p class="order_message">도서산간지역의 경우 추후 수령 시 추가 배송비가 발생할 수 있으며, 해외배송은 불가합니다.</p>
-            <p class="order_warning">배송지 불분명 및 수신불가 연락처 기입으로 반송되는 왕복 택배 비용은 구매자 부담으로 정확한 주소 및 통화 가능한 연락처 필수 기입<br>
-                대리주문으로 해외 주소로 발송 전, 주문품 꼭 확인해주세요. 오배송 및 불량 교환에 따른 배송비는 국내 택배 비용만 지원됩니다.</p>
-        </div>
-    </form>
-    <h4 class="subcategory">결제 정보</h4>
-    <form class="order_form">
-        <div class="row">
-            <input id="option_card" name="option" class="radio" value="card" type="radio">
-            <label for="option_card">신용카드</label>
-        </div>
-        <div class="row">
-            <input id="option_exchange" name="option" class="radio" value="exchange" type="radio">
-            <label for="option_exchange">무통장입금</label>
-        </div>
-    </form><br><br><br>
-    <h4 class="subcategory">추천인 정보</h4>
-    <form class="order_form">
-        <div class="row">
-            <p class="required">*</p>
-            <label for="name">추천인 아이디</label>
-            <input id="name" name="name" type="text" class="text" required/>
-            <div class="recommand_section">
+        <h3 class="category">ORDER</h3>
+        <h4 class="subcategory">주문자 정보</h4>
+        <p class="required_message"><span class="required">*</span>표시는 필수 입력 항목</p>
+        <form class="order_form">
+            <div class="row">
+                <p class="required">*</p>
+                <label for="name">주문자 이름</label>
+                <input id="name" name="name" type="text" class="text" required/>
             </div>
-        </div>
-    </form>
+            <div class="row pnumber">
+                <p class="required">*</p>
+                <label for="pnumber">연락처</label>
+                <input id="pnumber" name="pnumber_1" type="tel" class="tel" required/>
+                <span>-</span>
+                <input name="pnumber_2" type="tel" class="tel" required/>
+                <span>-</span>
+                <input name="pnumber_3" type="tel" class="tel" required/>
+            </div>
+            <div class="row">
+                <p class="required">*</p>
+                <label for="email">이메일</label>
+                <input id="email" name="email" type="email" class="email" required/>
+                <p class="email_message">이메일로 주문 진행상황을 안내해드립니다.</p>
+            </div>
+        </form>
+
+        <h4 class="subcategory">배송지 정보</h4>
+        <form class="order_form">
+            <div class="row">
+                <p class="required">*</p>
+                <label for="name">수령인 이름</label>
+                <input id="name" name="name" type="text" class="text" required/>
+            </div>
+            <div class="row pnumber">
+                <p class="required">*</p>
+                <label for="pnumber">연락처</label>
+                <input id="pnumber" name="pnumber_1" type="tel" class="tel" required/>
+                <span>-</span>
+                <input name="pnumber_2" type="tel" class="tel" required/>
+                <span>-</span>
+                <input name="pnumber_3" type="tel" class="tel" required/>
+            </div>
+            <div class="row pnumber">
+                <p>&nbsp;&nbsp;</p>
+                <label for="pnumber">추가 연락처</label>
+                <input id="pnumber" name="pnumber_1" type="tel" class="tel" required/>
+                <span>-</span>
+                <input name="pnumber_2" type="tel" class="tel" required/>
+                <span>-</span>
+                <input name="pnumber_3" type="tel" class="tel" required/>
+            </div>
+            <div class="row">
+                <p class="required">*</p>
+                <label for="name">배송지</label>
+                <input id="order" name="order" type="text" class="text" required/>
+                <input class="address_button" type="submit" value="주소 검색">
+                <div class="post_section"><input class="savepost" name="savepost" type="checkbox" checked=""> <label
+                            id="savemsg" for="savepost">기본 배송지로 설정</label></div>
+            </div>
+            <div class="row">
+                <input id="order_big" name="order" type="text" class="text" required/>
+            </div>
+            <div class="row">
+                <input id="order_big" name="order" type="text" class="text" placeholder="상세주소입력" required/>
+            </div>
+            <div class="row">
+                <p>&nbsp;&nbsp;</p>
+                <label for="name">배송 메세지</label>
+                <input id="order_bbig" name="order_msg" type="text" class="text" placeholder="20자 이내로 입력" required/>
+            </div>
+            <div class="row">
+                <p class="order_message">도서산간지역의 경우 추후 수령 시 추가 배송비가 발생할 수 있으며, 해외배송은 불가합니다.</p>
+                <p class="order_warning">배송지 불분명 및 수신불가 연락처 기입으로 반송되는 왕복 택배 비용은 구매자 부담으로 정확한 주소 및 통화 가능한 연락처 필수 기입<br>
+                    대리주문으로 해외 주소로 발송 전, 주문품 꼭 확인해주세요. 오배송 및 불량 교환에 따른 배송비는 국내 택배 비용만 지원됩니다.</p>
+            </div>
+        </form>
+        <h4 class="subcategory">결제 정보</h4>
+        <form class="order_form">
+            <div class="row">
+                <input id="option_card" name="option" class="radio" value="card" type="radio">
+                <label for="option_card">신용카드</label>
+            </div>
+            <div class="row">
+                <input id="option_exchange" name="option" class="radio" value="exchange" type="radio">
+                <label for="option_exchange">무통장입금</label>
+            </div>
+        </form><br><br><br>
+        <h4 class="subcategory">추천인 정보</h4>
+        <form class="order_form">
+            <div class="row">
+                <p class="required">*</p>
+                <label for="name">추천인 아이디</label>
+                <input id="name" name="name" type="text" class="text" required/>
+                <div class="recommand_section">
+                </div>
+            </div>
+        </form>
         <div class="order_button_cont">
             <button class="order_prev">뒤로가기</button>
             <button class="order_charge">결제하기</button>
         </div>
-</div>
+    </div>
 
-<footer>
-    <?= $this->loadLayout("footer") ?>
-</footer>
+    <footer>
+        <?= $this->loadLayout("footer") ?>
+    </footer>
 </body>
 </html>
