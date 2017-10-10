33cd65b58c90f1347fbaacb8b4b994802ec45a7d
SeongbumSeo <sBum.Seo@gmail.com>
2017-10-10 19:55:31 +0900

    creator.php

---
+++ creator.php
@@ -0,0 +1,10 @@
+<?php
+require("functions/template.php");
+
+$page = $_GET["page"];
+if (strlen($page) === 0)
+    $page = "home";
+
+$template = new Template();
+$template->loadView("pages/creator/$page");
+?>
