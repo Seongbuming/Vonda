beab9071852fa7f0c20f3cb5ca00a8906f1ec374
SiyeolBaek <qortlduf505@gmail.com>
2017-10-14 18:06:03 +0900

    상품 검색 결과 연동

--- views/pages/client/searchresults.php
+++ views/pages/client/searchresults.php
@@ -4,53 +4,39 @@
     <?=$this->loadLayout("head")?>
     <link rel="stylesheet" href="stylesheets/client/searchresults.css" />
 </head>
+<?php
 
+if (isset($_GET['keyword'])) {
+    $request = new Http();
+    $response = $request->request('GET', 'http://api.siyeol.com/search?query='.$_GET['keyword']);
+    $goods_result = $response->datas;
+}
+
+?>
 <body>
     <header>
         <?=$this->loadLayout("header")?>
     </header>
 
     <div id="contents">
-        <p class="numresults"><span class="keyword">브라운</span>에 대한 <span class="num">9</span>개의 검색결과</p>
+        <p class="numresults"><span class="keyword"><?=$_GET['keyword']?></span>에 대한 <span class="num"><?=sizeof($goods_result)?></span>개의 검색결과</p>
 
         <h3 class="category">Product</h3>
 
         <div class="product_section">
-            <a class="item overlay_container" href=".">
-                <img src="images/products/product1.png" alt="상품 이미지 1" />
-                <div class="overlay">
-                    <p class="product_name">일본의 국민로퍼</p>
-                    <p class="product_price">54,000won</p>
-                </div>
-            </a>
-            <a class="item overlay_container" href=".">
-                <img src="images/products/product2.png" alt="상품 이미지 2" />
-                <div class="overlay">
-                    <p class="product_name">일본의 국민로퍼</p>
-                    <p class="product_price">54,000won</p>
-                </div>
-            </a>
-            <a class="item overlay_container" href=".">
-                <img src="images/products/product3.png" alt="상품 이미지 3" style="margin-left: 50px;" />
-                <div class="overlay">
-                    <p class="product_name">일본의 국민로퍼</p>
-                    <p class="product_price">54,000won</p>
-                </div>
-            </a>
-            <a class="item overlay_container" href=".">
-                <img src="images/products/product4.png" alt="상품 이미지 4" />
-                <div class="overlay">
-                    <p class="product_name">일본의 국민로퍼</p>
-                    <p class="product_price">54,000won</p>
-                </div>
-            </a>
-            <a class="item overlay_container" href=".">
-                <img src="images/products/product5.png" alt="상품 이미지 5" />
-                <div class="overlay">
-                    <p class="product_name">일본의 국민로퍼</p>
-                    <p class="product_price">54,000won</p>
-                </div>
-            </a>
+            <?php
+            foreach ($goods_result as $goods) {
+            ?>
+                <a class="item overlay_container" href="./?page=product_detail&id=<?=$goods->id?>">
+                    <img src="http://api.siyeol.com/<?=$goods->goods_image?>" alt="상품 이미지 1" />
+                    <div class="overlay">
+                        <p class="product_name"><?=$goods->title?></p>
+                        <p class="product_price"><?=number_format($goods->options[0]->price)?>won</p>
+                    </div>
+                </a>
+            <?php
+            }
+            ?>
         </div>
 
         <h3 class="category">Creator</h3>
