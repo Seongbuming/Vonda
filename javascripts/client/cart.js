7f3878baf99596ed7993a8271749a015d74e1f9d
SiyeolBaek <qortlduf505@gmail.com>
2017-10-18 04:54:34 +0900

    장바구니 수량 변경 누락 파일

--- javascripts/client/cart.js
+++ javascripts/client/cart.js
@@ -2,10 +2,28 @@ $(document).ready(function() {
     $(".product_amount button").click(function() {
         var $amount = $(this).parent().find(".amount");
         var amount = parseInt($amount.text());
+        var goods_option_id = $(this).parent().parent().parent().find(".goods_option_id").val();
+        var cart_id = $(this).parent().parent().parent().find("[name='select_item[]']").val();
+        var goods_id = $(this).parent().parent().parent().find(".goods_id").val();
         if ($(this).is(".add") === true)
             $amount.text(amount+1);
         else if ($(this).is(".sub") === true && amount > 1)
             $amount.text(amount-1);
+
+        $.ajax({
+          type: "POST",
+          url: "http://api.siyeol.com/cart/"+cart_id+"?token="+readCookie('token'),
+          dataType: "json",
+          data: {'_method':'PUT', 'goods_id':goods_id, 'ea':$amount.text(), 'goods_option_id':goods_option_id},
+          success: function (res) {
+            if (res.code != 200) {
+            	alert("장바구니 업데이트에 실패하였습니다.");
+            }
+          },
+          error: function (err) {
+            alert("알수없는 오류입니다.\n관리자에게 문의하세요.");
+          }
+        });
     });
 
     $(".cart_delete").click(function() {
