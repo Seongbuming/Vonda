4cfa13866c5710b5795480b24bd719c45664d110
SiyeolBaek <qortlduf505@gmail.com>
2017-10-18 01:30:05 +0900

    결제 및 결제 완료 페이지 연동

--- views/pages/client/order.php
+++ views/pages/client/order.php
@@ -17,13 +17,13 @@ require($_SERVER['DOCUMENT_ROOT']."/libraries/NICEPAY/lib/NicepayLite.php");
 $nicepay = new NicepayLite;
 $request = new Http();
 
-$params = array( "items"=>array( array("goods_option_id"=>5, "ea"=>12), array("goods_option_id"=>4, "ea"=>12) ) );
+$params = array( "items"=>array( array("goods_option_id"=>5, "ea"=>12), array("goods_option_id"=>4, "ea"=>4), array("goods_option_id"=>6, "ea"=>1) ) );
 
 $response = $request->request('POST', 'http://api.siyeol.com/order?token='.$_COOKIE['token'], ['json' => $params]);
 
-$order = $response->datas;
+print_r($response);
 
-print_r($order);
+$order = $response->datas;
 
 $nicepay->m_MerchantKey = "EYzu8jGGMfqaDEp76gSckuvnaHHu+bC4opsSN6lHv3b2lurNYkVXrZ7Z1AoqQnXI3eLuaUFyoRNC6FkrzVjceg=="; // 상점키
 $nicepay->m_MID         = "nicepay00m";                         // 상점아이디
@@ -58,9 +58,21 @@ $hashString = bin2hex(hash('sha256', $nicepay->m_EdiDate.$nicepay->m_MID.$nicepa
     <?= $this->loadLayout("head") ?>
     <link rel="stylesheet" href="stylesheets/client/order.css"/>
 </head>
+<script src="libraries/jquery-3.2.1.min.js"></script>
 <script src="https://web.nicepay.co.kr/flex/js/nicepay_tr_utf.js" type="text/javascript"></script>
 <script type="text/javascript">
 //결제창 최초 요청시 실행됩니다.
+function pay(){
+    var $form = $("#form");
+
+    if ($form[0].checkValidity() == false) {
+        $(".check_button").click();
+    } else {
+        nicepayStart();
+    }
+    return false;
+}
+
 function nicepayStart(){
     goPay(document.payForm);
 }
@@ -93,38 +105,48 @@ function nicepayClose(){
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>
-                        <div class="product_img">
-                            <img src="images/products/product1.png" alt="상품사진"/>
-                        </div>
-                        <div class="product_info">
-                            <p class="creator"><a href=".">@Creator</a></p>
-                            <p class="name"><a href=".">SINGLE-BREASTED OVERSIZED BLAZER</a></p>
-                            <p class="type">실버</p>
-                        </div>
-                    </td>
-                    <td class="product_amount">1</td>
-                    <td class="product_shippingfee">2,500원</td>
-                    <td class="product_price">26,000원</td>
-                </tr>
+                <?php
+                foreach ($order->items as $item) {
+                ?>
+                    <tr>
+                        <td>
+                            <div class="product_img">
+                                <img src="http://api.siyeol.com/<?=$item->info->image?>" alt="상품사진"/>
+                            </div>
+                            <div class="product_info">
+                                <!-- <p class="creator"><a href=".">@Creator</a></p> -->
+                                <p class="name"><a href="."><?=$item->info->goods_name?></a></p>
+                                <?php
+                                if ($item->info->option) {
+                                    echo '<p class="type">'.$item->info->option.'</p>';
+                                }
+                                ?>
+                            </div>
+                        </td>
+                        <td class="product_amount"><?=$item->ea?></td>
+                        <td class="product_shippingfee"><?=number_format($item->shipping_charge)?>원</td>
+                        <td class="product_price"><?=number_format($item->price * $item->ea)?>원</td>
+                    </tr>
+                <?php
+                }
+                ?>
             </tbody>
         </table>
         <div class="sum_price">
             <div class="wrapper">
                 <div class="term">
                     <p class="caption">상품금액</p>
-                    <p class="price">26,000원</p>
+                    <p class="price"><?=number_format($order->origin_price)?>원</p>
                 </div>
                 <div class="operator">+</div>
                 <div class="term">
                     <p class="caption">배송비</p>
-                    <p class="price">2,500원</p>
+                    <p class="price"><?=number_format($order->shipping_price)?>원</p>
                 </div>
                 <div class="operator">=</div>
                 <div class="term">
                     <p class="caption">총 결제금액</p>
-                    <p class="price">28,500원</p>
+                    <p class="price"><?=number_format($order->total_price)?>원</p>
                 </div>
             </div>
         </div>
@@ -132,11 +154,11 @@ function nicepayClose(){
         <h3 class="category">ORDER</h3>
         <h4 class="subcategory">주문자 정보</h4>
         <p class="required_message"><span class="required">*</span>표시는 필수 입력 항목</p>
-        <form class="order_form" name="payForm" method="POST" action=".?page=order_finish">
+        <form id="form" class="order_form" name="payForm" method="POST" action=".?page=order_finish">
             <div class="row">
                 <p class="required">*</p>
                 <label for="name">주문자 이름</label>
-                <input id="name" name="name" type="text" class="text" required/>
+                <input id="name" name="BuyerName" type="text" class="text" required/>
             </div>
             <div class="row pnumber">
                 <p class="required">*</p>
@@ -160,8 +182,6 @@ function nicepayClose(){
             <input type="hidden" name="GoodsCnt" value="<?=$goodsCnt?>"></td>
             <!-- 결제 상품금액 -->
             <input type="hidden" name="Amt" value="<?php echo($nicepay->m_Price);?>"></td>
-            <!-- 구매자명 -->
-            <input type="hidden" name="BuyerName" value="<?php echo($nicepay->m_BuyerName);?>"></td>
             <!-- 구매자 연락처 -->
             <input type="hidden" name="BuyerTel" value="<?php echo($nicepay->m_BuyerTel);?>"></td>
             <!-- 상품 주문번호 -->
@@ -178,58 +198,55 @@ function nicepayClose(){
             <!-- 해쉬값 -->
             <input type="hidden" name="TrKey" value=""/>
             <!-- 필드만 필요 -->
-        </form>
 
         <h4 class="subcategory">배송지 정보</h4>
-        <form class="order_form">
             <div class="row">
                 <p class="required">*</p>
                 <label for="name">수령인 이름</label>
-                <input id="name" name="name" type="text" class="text" required/>
+                <input id="name" name="receive_name" type="text" class="text" required/>
             </div>
             <div class="row pnumber">
                 <p class="required">*</p>
                 <label for="pnumber">연락처</label>
-                <input id="pnumber" name="pnumber_1" type="tel" class="tel" required/>
+                <input id="pnumber" name="rnumber_1" type="tel" class="tel" required/>
                 <span>-</span>
-                <input name="pnumber_2" type="tel" class="tel" required/>
+                <input name="rnumber_2" type="tel" class="tel" required/>
                 <span>-</span>
-                <input name="pnumber_3" type="tel" class="tel" required/>
+                <input name="rnumber_3" type="tel" class="tel" required/>
             </div>
             <div class="row pnumber">
                 <p>&nbsp;&nbsp;</p>
                 <label for="pnumber">추가 연락처</label>
-                <input id="pnumber" name="pnumber_1" type="tel" class="tel" required/>
+                <input id="pnumber" name="anumber_1" type="tel" class="tel"/>
                 <span>-</span>
-                <input name="pnumber_2" type="tel" class="tel" required/>
+                <input name="anumber_2" type="tel" class="tel"/>
                 <span>-</span>
-                <input name="pnumber_3" type="tel" class="tel" required/>
+                <input name="anumber_3" type="tel" class="tel"/>
             </div>
             <div class="row">
                 <p class="required">*</p>
                 <label for="name">배송지</label>
-                <input id="order" name="order" type="text" class="text" required/>
+                <input id="order" name="zipcode" type="text" class="text" required/>
                 <input class="address_button" type="submit" value="주소 검색">
                 <div class="post_section"><input class="savepost" name="savepost" type="checkbox" checked=""> <label
                             id="savemsg" for="savepost">기본 배송지로 설정</label></div>
             </div>
             <div class="row">
-                <input id="order_big" name="order" type="text" class="text" required/>
+                <input id="order_big" name="address" type="text" class="text" required/>
             </div>
             <div class="row">
-                <input id="order_big" name="order" type="text" class="text" placeholder="상세주소입력" required/>
+                <input id="order_big" name="address_detail" type="text" class="text" placeholder="상세주소입력" required/>
             </div>
             <div class="row">
                 <p>&nbsp;&nbsp;</p>
                 <label for="name">배송 메세지</label>
-                <input id="order_bbig" name="order_msg" type="text" class="text" placeholder="20자 이내로 입력" required/>
+                <input id="order_bbig" name="delivery_memo" type="text" class="text" placeholder="20자 이내로 입력"/>
             </div>
             <div class="row">
                 <p class="order_message">도서산간지역의 경우 추후 수령 시 추가 배송비가 발생할 수 있으며, 해외배송은 불가합니다.</p>
                 <p class="order_warning">배송지 불분명 및 수신불가 연락처 기입으로 반송되는 왕복 택배 비용은 구매자 부담으로 정확한 주소 및 통화 가능한 연락처 필수 기입<br>
                     대리주문으로 해외 주소로 발송 전, 주문품 꼭 확인해주세요. 오배송 및 불량 교환에 따른 배송비는 국내 택배 비용만 지원됩니다.</p>
             </div>
-        </form>
         <!--
         <h4 class="subcategory">결제 정보</h4>
         <form class="order_form">
@@ -244,18 +261,18 @@ function nicepayClose(){
         </form><br><br><br>
         -->
         <h4 class="subcategory">추천인 정보</h4>
-        <form class="order_form">
             <div class="row">
-                <p class="required">*</p>
+                <!--<p class="required">*</p>-->
                 <label for="name">추천인 아이디</label>
-                <input id="name" name="name" type="text" class="text" required/>
+                <input id="name" name="recommender" type="text" class="text"/>
                 <div class="recommand_section">
                 </div>
             </div>
+        <input class="check_button" type="submit" value="" style="display: none">
         </form>
         <div class="order_button_cont">
             <button class="order_prev">뒤로가기</button>
-            <button class="order_charge" onClick="nicepayStart();">결제하기</button>
+            <button class="order_charge" onClick="pay();">결제하기</button>
         </div>
     </div>
 
--- views/pages/client/order_finish.php
+++ views/pages/client/order_finish.php
@@ -10,17 +10,19 @@
         <?= $this->loadLayout("header") ?>
     </header>
     <?php
-    print_r($_POST);
-    // Array ( [name] => [pnumber_1] => [pnumber_2] => [pnumber_3] => [email] => [PayMethod] => CARD [GoodsName] => 테스트 상품 외 1 [GoodsCnt] => 2 [Amt] => 96000 [BuyerName] => 나이스 [BuyerTel] => 01000000000 [Moid] => 201710170315193 [MID] => nicepay00m [UserIP] => 127.0.0.1 [EdiDate] => 20171017121518 [EncryptData] => 5e4c17c44442e162f8b2b9f3215c3e3f18cffdb7b32b24bdfbb3f065ed11489c [TrKey] => TRKYnicepay00m1710171216464946 [VerifySType] => S [EncGoodsName] => [EncBuyerName] => [JsVer] => nicepay_tr_utf [DeployedVer] => 1.1.1 [DeployedDate] => 170309 [DeployedFileName] => nicepay_tr_utf [AuthResultCode] => 0000 [AuthResultMsg] => 인증 성공 [BuyerEmail] => qortlduf505@gmail.com [TID] => nicepay00m01011179171216469712 [EncodeKey] => nicepay00m01011179171216469712 )
-
     if ($_POST["AuthResultCode"] != "0000") {
         // error
     }
 
+
+
     $request = new Http();
+    // Payment
     $response = $request->request('POST', 'http://api.siyeol.com/order/payment?token='.$_COOKIE['token'], ['json' => ['order_no' => $_POST['Moid'], 'tid' => $_POST['TID']]]);
 
-    echo "<br>";
+    // Info
+    $response = $request->request('POST', 'http://api.siyeol.com/order/info?token='.$_COOKIE['token'], ['json' => $_POST]);
+
     $order = $response->data;
 
     // order_no, TID, 
@@ -45,11 +47,11 @@
         <div class="order_line"></div>
         <div class="order_message">
             <p class="order_m"><span class="order_title">수령인</span><?=$order->delivery->receive_name?></p>
-            <p class="order_m"><span class="order_title">연락처</span>010-2344_****</p>
-            <p class="order_m"><span class="order_title">배송지</span>29123</p>
-            <p class="order_location"><span class="order_title"></span>인천광역시 연수구 선학동</p>
-            <p class="order_location"><span class="order_title"></span>********</p>
-            <p class="order_m"><span class="order_title">배송 메세지</span>-</p>
+            <p class="order_m"><span class="order_title">연락처</span><?=$order->delivery->receive_phone?></p>
+            <p class="order_m"><span class="order_title">배송지</span><?=$order->delivery->zipcode?></p>
+            <p class="order_location"><span class="order_title"></span><?=$order->delivery->address?></p>
+            <p class="order_location"><span class="order_title"></span><?=$order->delivery->address_detail?></p>
+            <p class="order_m"><span class="order_title"><?=$order->delivery->delivery_message?></span>-</p>
         </div>
         <div class="order_line"></div>
 
