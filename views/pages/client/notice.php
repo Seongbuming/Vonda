636881654feb477603d8c93d21f6c5e3f69c164c
SiyeolBaek <qortlduf505@gmail.com>
2017-10-18 02:32:08 +0900

    공지사항 연동

--- views/pages/client/notice.php
+++ views/pages/client/notice.php
@@ -4,7 +4,12 @@
     <?=$this->loadLayout("head")?>
     <link rel="stylesheet" href="stylesheets/board.css" />
 </head>
+<?php
+$request = new Http();
+$response = $request->request('GET', 'http://api.siyeol.com/notice');
 
+$notices = $response->datas;
+?>
 <body>
     <header>
         <?=$this->loadLayout("header")?>
@@ -17,66 +22,18 @@
 
         <table class="board">
             <tbody>
-                <tr class="row_subject">
-                    <td class="time">2017.08.22 13:20</td>
-                    <td class="subject">
-                        <a href=".?page=notice_detail">선선한 가을 날씨, 가디건 준비하세요(11)</a>
-                    </td>
-                </tr>
-                <tr class="row_subject">
-                    <td class="time">2017.08.21 13:20</td>
-                    <td class="subject">
-                        <a href=".?page=notice_detail">이제 이불 덮고 자요, 그러다 감기 걸려요 새 계절, 새 잠옷(25)</a>
-                    </td>
-                </tr>
-                <tr class="row_subject">
-                    <td class="time">2017.08.19 13:20</td>
-                    <td class="subject">
-                        <a href=".?page=notice_detail">간절기를 위한 가벼운 아우터(99)</a>
-                    </td>
-                </tr>
-                <tr class="row_subject">
-                    <td class="time">2017.08.18 13:20</td>
-                    <td class="subject">
-                        <a href=".?page=notice_detail">[ 안경展 ] 9 브랜드 최대 30% 할인(105)</a>
-                    </td>
-                </tr>
-                <tr class="row_subject">
-                    <td class="time">2017.08.17 13:20</td>
-                    <td class="subject">
-                        <a href=".?page=notice_detail">손목 위의 시간, 시계 기획전 최대 30% 할인(877)</a>
-                    </td>
-                </tr>
-                <tr class="row_subject">
-                    <td class="time">2017.08.22 13:20</td>
-                    <td class="subject">
-                        <a href=".?page=notice_detail">선선한 가을 날씨, 가디건 준비하세요(11)</a>
-                    </td>
-                </tr>
-                <tr class="row_subject">
-                    <td class="time">2017.08.21 13:20</td>
-                    <td class="subject">
-                        <a href=".?page=notice_detail">이제 이불 덮고 자요, 그러다 감기 걸려요 새 계절, 새 잠옷(25)</a>
-                    </td>
-                </tr>
-                <tr class="row_subject">
-                    <td class="time">2017.08.19 13:20</td>
-                    <td class="subject">
-                        <a href=".?page=notice_detail">간절기를 위한 가벼운 아우터(99)</a>
-                    </td>
-                </tr>
-                <tr class="row_subject">
-                    <td class="time">2017.08.18 13:20</td>
-                    <td class="subject">
-                        <a href=".?page=notice_detail">[ 안경展 ] 9 브랜드 최대 30% 할인(105)</a>
-                    </td>
-                </tr>
-                <tr class="row_subject">
-                    <td class="time">2017.08.17 13:20</td>
-                    <td class="subject">
-                        <a href=".?page=notice_detail">손목 위의 시간, 시계 기획전 최대 30% 할인(877)</a>
-                    </td>
-                </tr>
+                <?php
+                foreach ($notices->data as $notice) {
+                ?>
+                    <tr class="row_subject">
+                        <td class="time"><?=$notice->created_at?></td>
+                        <td class="subject">
+                            <a href=".?page=notice_detail&id=<?=$notice->id?>"><?=$notice->subject?></a>
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
             </tbody>
         </table>
 
