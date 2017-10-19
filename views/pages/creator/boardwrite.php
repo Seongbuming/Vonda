a1d87d8b2091accedd70c3b29c860a1b78a48d6d
hanboram.jang <janghan3150@gmail.com>
2017-10-19 15:19:49 +0900

    creator/boardwrite 텍스트 에디터 삽입 - 텍스트에디터(부트스트랩 필요) 때문에 레이아웃 틀어짐 수정.

--- views/pages/creator/boardwrite.php
+++ views/pages/creator/boardwrite.php
@@ -1,13 +1,22 @@
 <!doctype html>
 <html lang="ko">
 <head>
+  <link href="//netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
     <?=$this->loadLayout("head")?>
+
+    <script src="libraries/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.8/summernote.css" rel="stylesheet">
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.8/summernote.js"></script>
     <link rel="stylesheet" href="stylesheets/creator/board_write.css"/>
 </head>
 
 <body>
 <header>
     <?=$this->loadLayout("header")?>
+
 </header>
 
 <div id="contents">
@@ -19,11 +28,12 @@
     </ul>
     <h3 class="category">BOARD</h3>
     <input type="text" class="title" placeholder="제목을 입력해주세요.">
-    <div class="editor">
-        <!-- editor 영역
-        http://libcodes.com/codes/wysiwyg-html-editor-bootstrap-based-rich-text-editor
-        -->
+    <div class="editor-container" style="width:1155px;margin:auto;">
+      <div id="text-editor" class="form-item text-center"
+            style="width:100%; height: 350px; border: solid 1px black;">
+      </div>
     </div>
+
     <div class="write_cont">
         <button class="write"><a href="#">완료</a></button>
     </div>
@@ -31,6 +41,8 @@
 
 <footer>
     <?=$this->loadLayout("footer")?>
+
 </footer>
+<script src="javascripts/admin/text_editor.js"></script>
 </body>
 </html>
