0b6109a103de991435fb90896c5d5021de6198d3
hanboram.jang <janghan3150@gmail.com>
2017-10-20 16:49:27 +0900

    creator/myproduct  수정, myproduct 홍보신청 modal

--- views/pages/creator/myproduct.php
+++ views/pages/creator/myproduct.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="stylesheets/seller/common.css?v=3">
     <link rel="stylesheet" href="stylesheets/creator/myproduct.css">
     <link rel="stylesheet" href="stylesheets/board.css">
+    <link rel="stylesheet" href="stylesheets/modal.css">
 </head>
 
 <body>
@@ -19,10 +20,42 @@
         <li><a href="./creator.php?page=profile">프로필 관리</a></li>
         <li><a href="./creator.php?page=calculate">정산내역</a></li>
     </ul>
+    <button type="button" name="button" class="btn-promotion-apply">신청하기</button>
+    <h3 class="category">홍보 신청</h3>
+    <table id="table-promotion-apply"class="order_list">
+        <thead>
+        <tr>
+            <th>신청날짜</th>
+            <th>신청상품</th>
+            <th>판매자</th>
+            <th>상태</th>
+        </tr>
+        </thead>
+        <tbody>
+        <td class="apply-date" rowspan="2">
+            2017.09.10
+        </td>
 
-    <div class="chart">
-        <!-- home.css chart 클래스 height 쥐어줌 -->
-    </div>
+        <td class="product">
+            <div class="product_img">
+                <img src="images/products/product3.png" alt="상품사진" />
+            </div>
+            <div class="product_info">
+                <p class="open product_detail">SINGLE-BREASTED OVERSIZED BLAZER</p>
+            </div>
+        </td>
+        <td class="seller">
+            <p>kikiki</p>
+        </td>
+        <td class="state">
+            <p class="state-text">승인대기</p>
+            <p >
+              <button type="button" name="btn-cancel-apply" class="btn-cancel-apply">신청취소</button>
+            </p>
+        </td>
+        </tr>
+        </tbody>
+    </table>
 
     <h3 class="category">총 상품 9개</h3>
     <table class="order_list">
@@ -65,10 +98,65 @@
         </tr>
         </tbody>
     </table>
+
+    <div id="modal_promotion_apply" class="modal actived">
+        <div class="close_section modal_close"></div>
+        <div class="modal_body">
+            <button class="close_button modal_close">
+                <img src="images/buttons/close.png" alt="닫기" />
+            </button>
+
+            <p class="title">
+              상품검색
+            </p>
+
+            <div class="area_search">
+
+                <input class="search input_search" type="text" title="검색 입력" />
+                <button class="search submit_search" title="검색"></button>
+            </div>
+
+            <table class="order_list">
+
+                <tbody>
+                  <?php for ($i=0; $i < 5 ; $i++) {
+                    ?>
+                    <tr>
+                      <td class="">
+                        <input id="<?="select_".$i?>" class="item-checkbox" type="checkbox" title="선택">
+                        <label for="<?="select_".$i?>"></label>
+                      </td>
+                        <td class="product">
+                            <div class="product_img">
+                                <img src="images/products/product1.png" alt="상품사진" />
+                            </div>
+                            <div class="product_info">
+                                <p>SINGLE-BREASTED OVERSIZED BLAZER</p>
+                            </div>
+                        </td>
+                        <td class="seller">
+                          kikiki
+                        </td>
+                        <td class="order_price">
+                            <p>28,500원</p>
+                        </td>
+                    </tr>
+                    <?php
+                  }
+                  ?>
+                </tbody>
+            </table>
+
+            <button class="submit">신청하기</button>
+        </div>
+    </div>
+
 </div>
 
 <footer>
     <?=$this->loadLayout("creator/footer")?>
+    <script src="libraries/jquery-3.2.1.min.js"></script>
+    <script src="javascripts/modal.js"></script>
 </footer>
 </body>
 </html>
