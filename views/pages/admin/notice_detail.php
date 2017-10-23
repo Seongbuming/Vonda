c86b0a85741019bb402c20f54bb1b9369b5d0258
SiyeolBaek <qortlduf505@gmail.com>
2017-10-23 09:35:48 +0900

    공지사항 목록, 상세페이지 연동

--- views/pages/admin/notice_detail.php
+++ views/pages/admin/notice_detail.php
@@ -14,7 +14,17 @@
     <link rel="stylesheet" href="stylesheets/admin/notice_detail.css"/>
 
 </head>
+<?php
+if (!isset($_GET['id'])) {
+  header('Location:./admin.php?page=notice');
+}
+$type = $_GET['type'] ? $_GET['type'] : 'customer';
 
+$request = new Http();
+$response = $request->request('GET', '/board/'.$_GET['id']);
+
+$notice = $response->data;
+?>
 <body>
 
     <div id="wrapper" class="toggled">
@@ -32,13 +42,13 @@
               <!-- Nav tabs -->
               <ul class="nav">
                 <li class="nav-item">
-                  <a class="nav-link active" href="admin.php?page=notice_customer">CUSTOMER</a>
+                  <a class="nav-link <?=$type == 'customer' ? 'active' : ''?>"  href="admin.php?page=notice&type=customer" >CUSTOMER</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="admin.php?page=notice_seller">SELLER</a>
+                  <a class="nav-link <?=$type == 'seller' ? 'active' : ''?>"  href="admin.php?page=notice&type=seller">SELLER</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="admin.php?page=notice_creator">CREATOR</a>
+                  <a class="nav-link <?=$type == 'creator' ? 'active' : ''?>"  href="admin.php?page=notice&type=creator">CREATOR</a>
                 </li>
                 <div class="input-container">
                   <input type="input" name="search_input" value="" placeholder="">
@@ -54,31 +64,15 @@
                     <img src="images/buttons/admin/plus.png" alt="plus.png" />
                   </a>
                   <h4 class="title text-heavy-gray">
-                    [브랜드 오픈]상반된 개념의 믹스앤 매치로 차별화된 컬렉션을 선보이는 레이블, PUSHBUTTO
+                    <?=$notice->subject?>
                   </h4>
                   <p class="date">
-                    2017.08.22 13:20
+                    <?=str_replace("-", ".", substr($notice->created_at, 0, 16))?>
                   </p>
 
                     <div class="contents">
                       <!-- 텍스트 에디터에 의해서 생성된 내용 -->
-                      <div class="cnt-news-wrap ga-view" id="newsView" itemprop="articleBody">
-                				<div class="summary">
-                          '전자 소그룹 컨트롤타워' 역할<span name="inspace_pos">&nbsp;</span><br style="">
-                          부사장급 이하 조기 인사 검토
-                        </div>
-                				삼성전자가 삼성디스플레이 삼성SDI 삼성전기 삼성SDS 등 전자 계열사의 전략 및 인사 업무를 총괄하는 방안을 추진한다.
-                        그룹 컨트롤타워인 미래전략실이 지난 3월 해체되면서 계열사 간 사업 조정이나 고위 임원 인사 등 핵심 업무가 차질을 빚고 있다는 판단에서다.
-                        지난해 ‘최순실 국정농단’ 사태로 흐트러진 조직을 쇄신하기 위한 조기 임원 인사도 검토하고 있는 것으로 알려졌다.
-
-                        <span name="inspace_pos">&nbsp;</span><br style=""><br style="">
-
-                				삼성 계열사의 한 고위 관계자는 11일 “삼성전자가 자회사들의 사업 조정 및 구조조정,
-                        임원 인사 등 전략과 인사 업무를 총괄하는 여러 방안을 검토하고 있다”고 말했다.
-                        삼성전자는 삼성디스플레이(84.8%) 삼성SDI(19.6%) 삼성전기(23.7%) 등 전자 계열사의 최대 주주다.
-                        지난 3월 해체된 미래전략실 같은 별도 조직을 구성하는 대신 삼성전자가 주요 자회사를 통합 관리하는 지배구조를 유력하게 검토하고 있다.
-                        이사회 중심의 선진 기업 지배구조를 확립하겠다는 이재용 삼성전자 부회장의 뜻도 반영된 것으로 알려졌다.
-                        삼성그룹이 삼성전자 삼성생명 삼성물산 등 주요 계열사를 축으로 한 소그룹 중심으로 재편될 것이라는 관측이 나온다.
+                      <?=$notice->content?>
 
                     </div>
                   </div>
