c21a509b7bf9b3ee006059f2fa9ec878424c05ad
SiyeolBaek <qortlduf505@gmail.com>
2017-10-24 02:39:05 +0900

    로그 디렉토리 설정 및 결과 확인 추가 + 정상 상점키 등록 후 재 테스트 필요

--- views/pages/client/order_finish.php
+++ views/pages/client/order_finish.php
@@ -10,6 +10,61 @@
         <?= $this->loadLayout("header") ?>
     </header>
     <?php
+    require($_SERVER['DOCUMENT_ROOT']."/libraries/NICEPAY/lib/NicepayLite.php");
+
+    $nicepay                  = new NicepayLite;
+    $MerchantKey              = "EYzu8jGGMfqaDEp76gSckuvnaHHu+bC4opsSN6lHv3b2lurNYkVXrZ7Z1AoqQnXI3eLuaUFyoRNC6FkrzVjceg=="; // 상점키
+    $nicepay->m_NicepayHome   = $_SERVER['DOCUMENT_ROOT']."/libraries/NICEPAY/log";               // 로그 디렉토리 설정
+    $nicepay->m_ActionType    = "PYO";                  // ActionType
+    $nicepay->m_charSet       = "UTF8";                 // 인코딩
+    $nicepay->m_ssl           = "true";                 // 보안접속 여부
+    $nicepay->m_Price         = $_POST['Amt'];                   // 금액
+    $nicepay->m_NetCancelAmt  = $_POST['Amt'];                   // 취소 금액
+    $nicepay->m_NetCancelPW   = "123456";               // 결제 취소 패스워드 설정   
+
+    /*
+    *******************************************************
+    * <결제 결과 필드>
+    *******************************************************
+    */
+    $nicepay->m_BuyerName     = $_POST['BuyerName'];             // 구매자명
+    $nicepay->m_BuyerEmail    = $_POST['BuyerEmail'];            // 구매자이메일
+    $nicepay->m_BuyerTel      = $_POST['BuyerTel'];              // 구매자연락처
+    //$nicepay->m_EncryptedData = $EncryptedData'];         // 해쉬값
+    $nicepay->m_GoodsName     = $_POST['GoodsName'];             // 상품명
+    $nicepay->m_GoodsCnt      = $_POST['m_GoodsCnt'];            // 상품개수
+    $nicepay->m_GoodsCl       = $_POST['GoodsCl'];               // 실물 or 컨텐츠
+    $nicepay->m_Moid          = $_POST['Moid'];                  // 주문번호
+    $nicepay->m_MallUserID    = $_POST['MallUserID'];            // 회원사ID
+    $nicepay->m_MID           = $_POST['MID'];                   // MID
+    $nicepay->m_MallIP        = $_POST['MallIP'];                // Mall IP
+    $nicepay->m_MerchantKey   = $_POST['MerchantKey'];           // 상점키
+    $nicepay->m_LicenseKey    = $_POST['MerchantKey'];           // 상점키
+    $nicepay->m_TransType     = $_POST['TransType'];             // 일반 or 에스크로
+    $nicepay->m_TrKey         = $_POST['TrKey'];                 // 거래키
+    $nicepay->m_PayMethod     = $_POST['PayMethod'];             // 결제수단
+    $nicepay->startAction();
+        
+    /*
+    *******************************************************
+    * <결제 성공 여부 확인>
+    *******************************************************
+    */  
+    $resultCode = $nicepay->m_ResultData["ResultCode"];
+
+    $paySuccess = false;
+    if($PayMethod == "CARD"){
+        if($resultCode == "3001") $paySuccess = true;   // 신용카드(정상 결과코드:3001)
+    }else if($PayMethod == "BANK"){
+        if($resultCode == "4000") $paySuccess = true;   // 계좌이체(정상 결과코드:4000)
+    }else if($PayMethod == "CELLPHONE"){
+        if($resultCode == "A000") $paySuccess = true;   // 휴대폰(정상 결과코드:A000)
+    }else if($PayMethod == "VBANK"){
+        if($resultCode == "4100") $paySuccess = true;   // 가상계좌(정상 결과코드:4100)
+    }else if($payMethod == "SSG_BANK"){
+        if($resultCode == "0000") $paySuccess = true;   // SSG은행계좌(정상 결과코드:0000)
+    }
+
     if ($_POST["AuthResultCode"] != "0000") {
         // error
         echo "<script>error();</script>";
