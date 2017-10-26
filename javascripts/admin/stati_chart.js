b832a104466d746c1818ee47b696703501d36f2f
hanboram.jang <janghan3150@gmail.com>
2017-10-27 00:47:28 +0900

    필요없는 함수 삭제.

--- javascripts/admin/stati_chart.js
+++ javascripts/admin/stati_chart.js
@@ -198,21 +198,6 @@ function getData(type){
   });
 }
 
-/**
- * @param {int} The month number, 0 based
- * @param {int} The year, not zero based, required to account for leap years
- * @return {Date[]} List with date objects for each day of the month
- */
-function getDaysInMonth(month, year) {
-     var date = new Date(year, month, 1);
-     var days = [];
-     while (date.getMonth() === month) {
-        days.push(date.getDate());
-        date.setDate(date.getDate() + 1);
-     }
-     return days;
-}
-
 function getDateStr(myDate){
 	return (myDate.getFullYear() + '-' + (myDate.getMonth() + 1) + '-' + myDate.getDate());
 }
